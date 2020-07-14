090b37f3ec9258941508d305084d9876ebf3153f
Leonardo <leoat12@gmail.com>
2020-07-14 20:40:43 -0300

    Initial commit

---
+++ src/main/java/br/edu/infnet/loanmanagersystem/LoanManagerSystemApplication.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.loanmanagersystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanManagerSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoanManagerSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/edu/infnet/loanmanagersystem/LoanManagerSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.loanmanagersystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanManagerSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
