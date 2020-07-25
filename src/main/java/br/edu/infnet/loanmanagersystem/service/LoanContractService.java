7fe89d20b1de645f062867223c9b6449c5d56c29
Leonardo <leoat12@gmail.com>
2020-07-25 18:52:29 -0300

    Mover método para o devido serviço.

--- src/main/java/br/edu/infnet/loanmanagersystem/service/LoanContractService.java
+++ src/main/java/br/edu/infnet/loanmanagersystem/service/LoanContractService.java
@@ -1,10 +1,15 @@
 package br.edu.infnet.loanmanagersystem.service;
 
+import br.edu.infnet.loanmanagersystem.exception.ClientNotFoundException;
+import br.edu.infnet.loanmanagersystem.exception.CollectorNotFoundException;
 import br.edu.infnet.loanmanagersystem.model.LoanContract;
+import br.edu.infnet.loanmanagersystem.repository.ClientRepository;
+import br.edu.infnet.loanmanagersystem.repository.CollectorRepository;
 import br.edu.infnet.loanmanagersystem.repository.LoanContractRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Service
@@ -12,9 +17,24 @@ import java.util.List;
 public class LoanContractService {
 
     private final LoanContractRepository loanContractRepository;
+    private final ClientRepository clientRepository;
+    private final CollectorRepository collectorRepository;
 
     public List<LoanContract> findAll() {
         return loanContractRepository.findAll();
     }
 
+    public LoanContract registerLoanContract(Integer clientId, Integer collectorId,
+                                             Double amountGiven, Double interestRate) {
+
+        LoanContract contract = LoanContract.builder()
+                .client(clientRepository.findById(clientId).orElseThrow(ClientNotFoundException::new))
+                .collector(collectorRepository.findById(collectorId).orElseThrow(CollectorNotFoundException::new))
+                .amountGiven(amountGiven)
+                .interestRate(interestRate)
+                .dateLoanGiven(LocalDate.now())
+                .build();
+
+        return loanContractRepository.save(contract);
+    }
 }
--- src/main/java/br/edu/infnet/loanmanagersystem/service/PaymentService.java
+++ src/main/java/br/edu/infnet/loanmanagersystem/service/PaymentService.java
@@ -1,18 +1,11 @@
 package br.edu.infnet.loanmanagersystem.service;
 
-import br.edu.infnet.loanmanagersystem.exception.ClientNotFoundException;
-import br.edu.infnet.loanmanagersystem.exception.CollectorNotFoundException;
-import br.edu.infnet.loanmanagersystem.model.LoanContract;
 import br.edu.infnet.loanmanagersystem.model.Payment;
 import br.edu.infnet.loanmanagersystem.model.PaymentType;
-import br.edu.infnet.loanmanagersystem.repository.ClientRepository;
-import br.edu.infnet.loanmanagersystem.repository.CollectorRepository;
-import br.edu.infnet.loanmanagersystem.repository.LoanContractRepository;
 import br.edu.infnet.loanmanagersystem.repository.PaymentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
 import java.util.List;
 
 @Service
@@ -20,23 +13,6 @@ import java.util.List;
 public class PaymentService {
 
 	private final PaymentRepository paymentRepository;
-	private final ClientRepository clientRepository;
-	private final CollectorRepository collectorRepository;
-	private final LoanContractRepository loanContractRepository;
-
-	public LoanContract registerLoanContract(Integer clientId, Integer collectorId,
-											 Double amountGiven, Double interestRate) {
-
-		LoanContract contract = LoanContract.builder()
-				.client(clientRepository.findById(clientId).orElseThrow(ClientNotFoundException::new))
-				.collector(collectorRepository.findById(collectorId).orElseThrow(CollectorNotFoundException::new))
-				.amountGiven(amountGiven)
-				.interestRate(interestRate)
-				.dateLoanGiven(LocalDate.now())
-				.build();
-
-		return loanContractRepository.save(contract);
-	}
 	
 	public Payment makePayment(Payment payment) {
 		
@@ -57,8 +33,7 @@ public class PaymentService {
 		if(payment.getAmount() < interestValue) {
 			throw new IllegalArgumentException("The minumum value for payment is the interest value of the total due");
 		}
-		
-		
+
 		if(Double.compare(payment.getAmount() , interestValue) == 0) {
 			System.out.println("interest");
 			payment.setPaymentType(PaymentType.INTEREST);
